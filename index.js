5676bef19a77ce2223fb69c8f25d8babb6777eef
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2024-05-02 06:27:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function onBuilb(){
+    var builb = document.getElementById("builb");
+    builb.src = "bulb on.jpg"
+}
+function offBuilb(){
+    var builb = document.getElementById("builb");
+    builb.src = "bulb off.jpg"
+}
